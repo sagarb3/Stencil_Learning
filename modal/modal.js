b695951c21bb2d775cab06baf1b0609082f1c427
Sagar Bhattacharya <sagar13912@gmail.com>
2019-04-21 20:24:39 +0530

    Updated Readme | Slot change event listener , event bubbles

--- modal/modal.js
+++ modal/modal.js
@@ -27,7 +27,7 @@ class Modal extends HTMLElement {
         #modal {
             z-index:100;
             position:fixed;
-            top:15vh;
+            top:10vh;
             left:25%;
             width:50%;
             background:white;
@@ -38,6 +38,7 @@ class Modal extends HTMLElement {
             justify-content:space-between;
             opacity:0;
             pointer-events:none;
+            transition:all 0.3s ease-out;
         }
         #actions {
             border-top:1px solid #ccc;
@@ -61,6 +62,11 @@ class Modal extends HTMLElement {
             opacity:1;
             pointer-events:all;
         }
+
+        :host([opened]) #modal {
+          top:15vh;
+        }
+
         ::slotted(h1){
             font-size:1.2rem;
         }
@@ -76,11 +82,28 @@ class Modal extends HTMLElement {
             <slot></slot>
         </section>
         <section id="actions">
-            <button>Cancel</button>
-            <button>Okay</button>
+            <button id="cancel-btn">Cancel</button>
+            <button id="confirm-btn">Okay</button>
         </section>
      </div>
     `;
+
+    /** listening to slot content updated */
+    const slots = this.shadowRoot.querySelector("slot");
+    slots[1].addEventListener("slotchange", () => {
+      console.dir(slots[1].assignedNodes());
+    });
+
+    const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+    const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+    const backdropClick = this.shadowRoot.querySelector("#backdrop");
+
+    /** backdrop and cancel same events */
+    cancelButton.addEventListener("click", this._cancel.bind(this));
+    backdropClick.addEventListener("click", this._cancel.bind(this));
+
+    /** confirm other event */
+    confirmButton.addEventListener("click", this._confirm.bind(this));
   }
   connectedCallback() {}
 
@@ -108,11 +131,21 @@ class Modal extends HTMLElement {
       this.removeAttribute("opened");
     }
   }
-  _confirm() {
+  _confirm(event) {
     this.hide();
+    const confirmEvent = new Event("confirm");
+    // event.target.dispatchEvent(confirmEvent);
+    this.dispatchEvent(confirmEvent);
   }
-  _cancel() {
+  _cancel(event) {
     this.hide();
+    /** commeted is one way to do it */
+    // const cancelEvent = new Event("cancel",{bubbles:true,composed:true});
+    // event.target.dispatchEvent(cancelEvent);
+
+    /**proper way */
+    const cancelEvent = new Event("cancel");
+    this.dispatchEvent(cancelEvent);
   }
 }
 
